c235621b50c0f66e2a2f06cdc8692a09744bc6a3
Jake Abed <jakeabed@proton.me>
2024-10-27 20:31:22 -0700

    Create basic REPL with echoing

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
 import (
+  "os"
+  "bufio"
   "fmt"
 )
 
 func main() {
-  fmt.Println("Hello, World!")
+  running := true
+
+  fmt.Println("Pokedex booting up!")
+  fmt.Println("...")
+  fmt.Println("Welcome to the Pokedex!")
+
+  for running {
+    
+    scanner := bufio.NewScanner(os.Stdin)
+
+    fmt.Print("Pokedex > ")
+
+    scan := scanner.Scan()
+    if scan {
+      fmt.Println(scanner.Text())
+    }
+  }
 }
