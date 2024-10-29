1d00cb28f57954f679b6e555a9752bd2458c5818
Jake Abed <jakeabed@proton.me>
2024-10-29 14:17:37 -0700

    Add commands to CLI

--- main.go
+++ main.go
@@ -6,8 +6,45 @@ import (
   "fmt"
 )
 
+type cliCommand struct {
+  name string
+  description string
+  callback func() error
+}
+
+func buildCommands() map[string]cliCommand {
+  commands := map[string]cliCommand{
+    "help": {
+      name: "help",
+      description: "Displays a help message",
+      callback: commandHelp,
+    },
+    "exit": {
+      name: "exit",
+      description: "Exit the Pokedex",
+      callback: commandExit,
+    },
+  }
+  return commands
+}
+
+func commandExit() error {
+  fmt.Println("Exiting Pokedex... Goodbye for now!")
+  return nil
+}
+
+func commandHelp() error {
+  commands := buildCommands()
+  for _, v := range commands {
+    fmt.Println(fmt.Sprintf("%v - %v", v.name, v.description))
+  }
+  return nil
+}
+
 func main() {
   running := true
+  commands := buildCommands()
+
 
   fmt.Println("Pokedex booting up!")
   fmt.Println("...")
@@ -21,7 +58,19 @@ func main() {
 
     scan := scanner.Scan()
     if scan {
-      fmt.Println(scanner.Text())
+      command, ok := commands[scanner.Text()]
+      if ok {
+        switch command.name {
+        case "exit":
+          command.callback()
+          running = false
+        default:
+          command.callback()
+        }
+      } else {
+        fmt.Println("Command not found! Please enter 'help' for aid.")
+      }
     }
   }
 }
+
